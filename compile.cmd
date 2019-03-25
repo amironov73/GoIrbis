290b6e2c0c73d3bfce9e98a2cd0598ce2f72194c
Alexey Mironov <amironov73@gmail.com>
2019-03-26 07:46:55 +0800

    Fix build error

--- compile.cmd
+++ compile.cmd
@@ -4,4 +4,5 @@ SET GOEXECUTABLE=%GOROOT%\bin\go.exe
 SET SOURCE=%CD%\Source\goirbis.go
 SET OUTPUT=%CD%\Binaries\goirbis.exe
 
+%GOEXECUTABLE% get -u -v golang.org/x/text/encoding/charmap
 %GOEXECUTABLE% build -o %OUTPUT% -v -x -i %SOURCE%
