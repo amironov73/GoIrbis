08d9d903db8b84d804e86ef8b1ea4961715eae3d
Alexey Mironov <amironov73@gmail.com>
2019-03-26 07:39:25 +0800

    Fix build error

--- compile.cmd
+++ compile.cmd
@@ -4,4 +4,4 @@ SET GOEXECUTABLE=%GOROOT%\bin\go.exe
 SET SOURCE=%CD%\Source\goirbis.go
 SET OUTPUT=%CD%\Binaries\goirbis.exe
 
-%GOEXECUTABLE% build -o %OUTPUT% -v -x %SOURCE%
+%GOEXECUTABLE% build -o %OUTPUT% -v -x -i %SOURCE%
